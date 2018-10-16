eacc29b18e043d1b53650b2082943ba3a14ff355
Mraky09 <anhtnt09@gmail.com>
2018-10-16 13:07:38 +0700

    Authentication with token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::API
 
+  protected
+
+  def current_user
+    @current_user ||= User.from_access_token(access_token)
+  end
+
+  def authenticate_with_token!
+    return true if current_user
+    render json: {
+      _error: 'Token is invalid!'
+    }, :unauthorized
+  end
+
+  def access_token
+    authorization_header = request.headers['Authorization']
+    pattern = /^Bearer /
+    header.gsub(pattern, '') if header.match(pattern)
+  end
 end
